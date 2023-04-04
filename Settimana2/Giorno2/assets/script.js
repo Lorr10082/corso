215b9fb4b0c4b671fdaef85a9b357071402d8797
Lorr10082 <129152640+Lorr10082@users.noreply.github.com>
2023-04-04 17:03:38 +0200

    EsercizioJava3

---
+++ Settimana2/Giorno2/assets/script.js
@@ -0,0 +1,32 @@
+const btn = document.getElementById('verifica');
+var eta;
+var stato;
+
+btn.addEventListener('click', function() {
+    let nome = document.getElementById('nome');
+    let anno = document.getElementById('eta');
+
+    calcolaEta(anno.value);
+    verifica();
+    scrivi(nome.value);
+    cancellaForm(nome,anno);
+});
+
+function calcolaEta(anno) {
+    eta = 2023 - anno;
+}
+
+function verifica() {
+    stato=(eta >= 18) ? 'maggiorenne' : 'minorenne';
+}
+
+function scrivi(nome) {
+    document.getElementById('mioNome').innerHTML='Ciao '+nome;
+    document.getElementById('miaVerifica').innerHTML=' La tua età è ' + eta + ' anni; sei '+ stato;
+}
+
+function cancellaForm(nome,anno) {
+    nome.value='';
+    anno.value='';
+}
+
